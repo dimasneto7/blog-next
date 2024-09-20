f130abe7f7d5882491fd9a0c0ca6de98c8b2ba1e
dimasneto7 <dimas_fcn@outlook.com>
2024-09-20 11:29:03 -0300

    search

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Prompt } from 'next/font/google'
 import { Aside } from '@/components/Aside'
 import './globals.css'
+import { SearchForm } from '@/components/SearchForm'
 
 export const metadata = {
   title: 'Blog Next',
@@ -23,7 +24,10 @@ export default function RootLayout({ children }) {
           <div>
             <Aside />
           </div>
-          <div className="main-content">{children}</div>
+          <div className="main-content">
+            <SearchForm />
+            {children}
+          </div>
         </div>
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,20 @@ import Link from 'next/link'
 import db from '../../prisma/db'
 import styles from './page.module.css'
 
-async function getAllPosts(page) {
+async function getAllPosts(page, searchTerm) {
   try {
+    const where = {}
+
+    if (searchTerm) {
+      where.title = {
+        contains: searchTerm,
+        mode: 'insensitive',
+      }
+    }
+
     const perPage = 6
     const skip = (page - 1) * perPage
-    const totalItems = await db.post.count()
+    const totalItems = await db.post.count({ where })
     const totalPages = Math.ceil(totalItems / perPage)
     const prev = page > 1 ? page - 1 : null
     const next = page < totalPages ? page + 1 : null
@@ -16,6 +25,7 @@ async function getAllPosts(page) {
     const posts = await db.post.findMany({
       take: perPage,
       skip,
+      where,
       orderBy: { createdAt: 'desc' },
       include: {
         author: true,
@@ -31,15 +41,24 @@ async function getAllPosts(page) {
 
 export default async function Home({ searchParams }) {
   const currentPage = parseInt(searchParams?.page || 1)
-  const { data: posts, prev, next } = await getAllPosts(currentPage)
+  const searchTerm = searchParams?.q
+  const { data: posts, prev, next } = await getAllPosts(currentPage, searchTerm)
   return (
     <main className={styles.grid}>
       {posts.map((post) => (
         <CardPost key={post.id} post={post} />
       ))}
       <div className={styles.links}>
-        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
-        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+        {prev && (
+          <Link href={{ pathname: '/', query: { page: prev, q: searchTerm } }}>
+            Página anterior
+          </Link>
+        )}
+        {next && (
+          <Link href={{ pathname: '/', query: { page: next, q: searchTerm } }}>
+            Próxima página
+          </Link>
+        )}
       </div>
     </main>
   )
